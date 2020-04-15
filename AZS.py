ccb89dd8196b1fb5104f74a6e70db69f5eb61406
BesedaDi <dianabesedina223@gmail.com>
2020-04-15 21:46:10 +0700

    start

--- AZS.py
+++ AZS.py
@@ -1,1 +1,6 @@
-n = input()
+# Case-study #5 "Tessellation"
+# Developers: Besedina D. (%)
+#             Setskov M. (%)
+#             Moiseenko V. (%)
+# The aim: modeling the work of a gas station - calculating the required gasoline reserves of each brand.
+
